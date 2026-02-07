dd03d6c5f3fe78761846b12d3ff75399fe302965
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-07 16:19:18 +0000

    copied isRecursive from unsafe

---
+++ recursive.go
@@ -0,0 +1,63 @@
+package gotypes
+
+import "go/types"
+
+func IsTypeRecursive(typ types.Type) bool {
+	return isTypeRecursive(typ, map[types.Type]bool{})
+}
+
+func isTypeRecursive(typ types.Type, found map[types.Type]bool) bool {
+	f, ok := found[typ]
+	if ok {
+		return f
+	}
+
+	found[typ] = len(found) == 0
+
+	switch t := typ.Underlying().(type) {
+	case *types.Struct:
+		for field := range t.Fields() {
+			if isTypeRecursive(field.Type(), found) {
+				return true
+			}
+		}
+	case *types.Pointer:
+		return isTypeRecursive(t.Elem(), found)
+	case *types.Map:
+		if isTypeRecursive(t.Key(), found) {
+			return true
+		}
+
+		return isTypeRecursive(t.Elem(), found)
+	case *types.Array:
+		return isTypeRecursive(t.Elem(), found)
+	case *types.Slice:
+		return isTypeRecursive(t.Elem(), found)
+	case *types.Signature:
+		for typ := range t.Params().Variables() {
+			if isTypeRecursive(typ.Type(), found) {
+				return true
+			}
+		}
+
+		for typ := range t.Results().Variables() {
+			if isTypeRecursive(typ.Type(), found) {
+				return true
+			}
+		}
+	case *types.Interface:
+		for typ := range t.EmbeddedTypes() {
+			if isTypeRecursive(typ, found) {
+				return true
+			}
+		}
+
+		for fn := range t.ExplicitMethods() {
+			if isTypeRecursive(fn.Signature(), found) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
---
+++ recursive_test.go
@@ -0,0 +1,110 @@
+package gotypes
+
+import (
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"runtime"
+	"testing"
+)
+
+func TestIsRecursive(t *testing.T) {
+	for n, test := range [...]struct {
+		input       string
+		isRecursive bool
+	}{
+		{
+			"package a\n\ntype a struct { B b }\n\ntype b struct {c *b}",
+			false,
+		},
+		{
+			"package a\n\ntype a struct {B *a}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {B map[string]int}",
+			false,
+		},
+		{
+			"package a\n\ntype a struct {B map[*a]int}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {B map[string]a}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {B []a}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {B [2]*a}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {b func() int}",
+			false,
+		},
+		{
+			"package a\n\ntype a struct {b func() a}",
+			true,
+		},
+		{
+			"package a\n\ntype a struct {b func(int) }",
+			false,
+		},
+		{
+			"package a\n\ntype a struct {b func(a) }",
+			true,
+		},
+		{
+			"package a\n\ntype a struct { a b }\ntype b interface {C() int}",
+			false,
+		},
+		{
+			"package a\n\ntype a struct { a b }\ntype b interface {C() b}",
+			false,
+		},
+		{
+			"package a\n\ntype a interface { A() a }",
+			true,
+		},
+		{
+			"package a\n\ntype a interface { A() b }\ntype b interface { B() a\n}",
+			true,
+		},
+		{
+			"package a\n\ntype a interface { A() b }\ntype b struct { B a\n}",
+			true,
+		},
+	} {
+		if self := parseType(t, test.input); IsTypeRecursive(self) != test.isRecursive {
+			t.Errorf("test %d: didn't get expected recursive value: %v", n+1, test.isRecursive)
+		}
+	}
+}
+
+func parseType(t *testing.T, input string) types.Type {
+	t.Helper()
+
+	fset := token.NewFileSet()
+
+	f, err := parser.ParseFile(fset, "a.go", input, parser.AllErrors|parser.ParseComments)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	conf := types.Config{
+		GoVersion: runtime.Version(),
+		Importer:  importer.ForCompiler(fset, runtime.Compiler, nil),
+	}
+
+	pkg, err := conf.Check("a", fset, []*ast.File{f}, nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	return pkg.Scope().Lookup("a").Type()
+}

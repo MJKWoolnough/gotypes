e106bbfa9b55dd1c0984fae7c1aeaed2f0b55c7e
Michael Woolnough <michael.woolnough@gmail.com>
2026-02-08 19:43:15 +0000

    added tests for Lookup function

--- imports_test.go
+++ imports_test.go
@@ -24,3 +24,25 @@ func TestGetImports(t *testing.T) {
 		}
 	}
 }
+
+func TestLookup(t *testing.T) {
+	pkg, err := ParsePackage(".")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	imps := Imports(pkg)
+
+	for path, name := range map[string]string{
+		"vimagination.zapto.org/gotypes":      "Imports",
+		"vimagination.zapto.org/httpreaderat": "Request",
+		"vimagination.zapto.org/cache":        "LRU",
+		"archive/zip":                         "File",
+	} {
+		if typ, err := Lookup(imps, path+"."+name); err != nil {
+			t.Errorf("import %s: unexpected error: %s", path, err)
+		} else if typ.Name() != name {
+			t.Errorf("expecting package %q to have type name %q, got %q", path, name, typ.Name())
+		}
+	}
+}
